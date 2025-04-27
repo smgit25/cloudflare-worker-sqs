7140f3eb9aaaaa73bc8d8a7840cfb2758033859b
smgit25 <swatick27m@outlook.com>
2025-04-27 20:38:26 +0530

    test GA

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { sendMessageToSQS } from './sqs.js';
 
 export default {
   async fetch(request, env, ctx) {
+    
     const now = new Date();
     // const AWS_ACCESS_KEY_ID = env.AWS_ACCESS_KEY_ID;
     // const AWS_SECRET_ACCESS_KEY = env.AWS_SECRET_ACCESS_KEY;
